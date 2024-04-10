07883b48924697c0b293365abe94a9c222c129e1
Sergei Morozov <moroz051091@yandex.ru>
2024-04-10 18:47:04 +0400

    feat(utils): add debug mod

--- utils.py
+++ utils.py
@@ -3,17 +3,21 @@ import os
 from get_page import get_page
 
 
-def get_data(url: str) -> str:
+def get_data(url: str, debug: bool = False) -> str:
     """
     Функция для тестирования и отладки. Снижает количество запросов к сайту для избежания бана.
     Сохраняет результат запроса в файл index.html и дальнейшие запросы получения данных перенаправляются в этот файл.
     :param url:
+    :param debug:
     :return:
     """
-    if not os.path.exists("index.html"):
-        data = get_page(url)
-        write_html(data)
-    return read_html()
+    if debug:
+        if not os.path.exists("index.html"):
+            data = get_page(url)
+            write_html(data)
+        return read_html()
+
+    return get_page(url)
 
 
 def write_html(response: str):
