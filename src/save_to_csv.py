523d54962e78e7aa156842c85ab0bb6b147717e2
Sergei Morozov <moroz051091@yandex.ru>
2024-04-11 22:27:12 +0400

    fix bugs with path

--- src/save_to_csv.py
+++ src/save_to_csv.py
@@ -7,7 +7,7 @@ def save_new_file(data: dict):
 
 
 def add_data_to_file(data: dict):
-    df = pandas.read_csv('../test.csv')
+    df = pandas.read_csv('test.csv')
     for column_name, value in data.items():
         df[column_name] = value
     df.to_csv('test.csv', index=False, encoding='utf-8')
