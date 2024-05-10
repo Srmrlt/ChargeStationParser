a79291c1b578faf2e9afca9270bd5fefcbc53fa3
Sergei Morozov <moroz051091@yandex.ru>
2024-05-11 00:58:33 +0400

    feat(database): update station_status model and standardize model code style

--- src/database/database.py
+++ src/database/database.py
@@ -1,7 +1,5 @@
 from functools import wraps
-from typing import Annotated
 
-from sqlalchemy import String
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -9,16 +7,13 @@ from .config import settings
 
 engine = create_async_engine(
     url=settings.db_url,
-    echo=False,
+    echo=True,
 )
 
 session_factory = async_sessionmaker(engine, expire_on_commit=False)
 
-# Create a custom type annotation for string fields limited to 256 characters.
-str_256 = Annotated[str, 256]
 
-
-def session_manager(func):
+async def session_manager(func):
     """
     Decorates an async function to manage an SQLAlchemy session, handling
     creation, commit, and rollback. It injects a session as the first argument to the
@@ -51,8 +46,6 @@ class Base(DeclarativeBase):
     Define a base class for all SQLAlchemy Declarative models.
     It provides a common __repr__ method and a mapping from type annotations to SQLAlchemy types.
     """
-
-    type_annotation_map = {str_256: String(256)}
 
     def __repr__(self):
         return f"<{self.__class__.__name__}>"
--- src/database/models.py
+++ src/database/models.py
@@ -1,11 +1,11 @@
 import datetime
 from typing import Annotated
 
-from sqlalchemy import Column, ForeignKey, Integer, String, text
+from sqlalchemy import DateTime, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.schema import UniqueConstraint
+from sqlalchemy.schema import PrimaryKeyConstraint, UniqueConstraint
 
-from .database import Base, str_256
+from .database import Base
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
 created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
@@ -15,10 +15,10 @@ class StationInfoOrm(Base):
     __tablename__ = "station_info"
 
     id: Mapped[intpk]
-    number: Mapped[int] = Column(Integer, unique=True)
-    city: Mapped[str] = Column(String)
-    address: Mapped[str] = Column(String)
-    name: Mapped[str] = Column(String)
+    number: Mapped[int] = mapped_column(unique=True)
+    city: Mapped[str] = mapped_column(nullable=False)
+    address: Mapped[str]
+    name: Mapped[str]
     created_at: Mapped[created_at]
 
     station_socket: Mapped[list["StationSocketOrm"]] = relationship(back_populates="station_info")
@@ -26,14 +26,13 @@ class StationInfoOrm(Base):
 
 class StationSocketOrm(Base):
     __tablename__ = "station_socket"
+    __table_args__ = (UniqueConstraint("station_id", "socket", name="station_id_socket_unique"),)
 
     id: Mapped[intpk]
     station_id: Mapped[int] = mapped_column(ForeignKey("station_info.id", ondelete="CASCADE"))
-    socket: Mapped[str] = Column(String)
-    power: Mapped[int] = Column(Integer)
+    socket: Mapped[str]
+    power: Mapped[int]
     created_at: Mapped[created_at]
-
-    __table_args__ = (UniqueConstraint("station_id", "socket", name="station_id_socket_unique"),)
 
     station_info: Mapped["StationInfoOrm"] = relationship(back_populates="station_socket")
     station_status: Mapped[list["StationStatusOrm"]] = relationship(back_populates="station_socket")
@@ -41,10 +40,12 @@ class StationSocketOrm(Base):
 
 class StationStatusOrm(Base):
     __tablename__ = "station_status"
+    __table_args__ = (
+        PrimaryKeyConstraint("station_socket_id", "timestamp"),
+    )
 
-    id: Mapped[intpk]
     station_socket_id: Mapped[int] = mapped_column(ForeignKey("station_socket.id", ondelete="CASCADE"))
-    status: Mapped[str_256]
-    timestamp: Mapped[datetime.datetime]
+    status: Mapped[str]
+    timestamp: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
 
     station_socket: Mapped["StationSocketOrm"] = relationship(back_populates="station_status")
